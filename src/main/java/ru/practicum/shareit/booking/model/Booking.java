363d5e26ee91bb7c63e24d2ee5dbcfd249d9a732
S.Petrovskii <sergdest261276@yandex.ru>
2024-04-30 15:41:45 +0300

    feat: ТЗ-13 first commit add-controllers

---
+++ src/main/java/ru/practicum/shareit/booking/model/Booking.java
@@ -0,0 +1,31 @@
+package ru.practicum.shareit.booking.model;
+
+import lombok.Builder;
+import lombok.Data;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.PastOrPresent;
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+public class Booking {
+
+    @NotNull
+    private Long id;
+
+    private LocalDateTime start;
+
+    @PastOrPresent
+    private LocalDateTime finish;
+
+    @NotNull
+    private Long itemId;
+
+    @NotNull
+    private Long userId;
+
+    @NotBlank
+    private Status status;
+}
--- src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
+++ src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
@@ -1,7 +1,26 @@
 package ru.practicum.shareit.item.dto;
 
-/**
- * TODO Sprint add-controllers.
- */
+import lombok.Builder;
+import lombok.Data;
+import ru.practicum.shareit.marker.Marker;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Data
+@Builder
 public class ItemDto {
+    private Long id;
+
+    @NotBlank(groups = Marker.OnCreate.class)
+    @Size(min = 1, max = 20, groups = {Marker.OnCreate.class, Marker.OnUpdate.class})
+    private String name;
+
+    @NotBlank(groups = Marker.OnCreate.class)
+    @Size(min = 1, max = 40, groups = {Marker.OnCreate.class, Marker.OnUpdate.class})
+    private String description;
+
+    @NotNull(groups = Marker.OnCreate.class)
+    private Boolean available;
 }
