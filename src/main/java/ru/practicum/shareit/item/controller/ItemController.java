f59af261cb15865b3d73da8b7e1bad46afed9ac3
S.Petrovskii <sergdest261276@yandex.ru>
2024-05-02 13:10:12 +0300

    fix: ТЗ-13 add constant for Request Header -step3 add static modificator for HEADER constant

--- src/main/java/ru/practicum/shareit/item/controller/ItemController.java
+++ src/main/java/ru/practicum/shareit/item/controller/ItemController.java
@@ -21,24 +21,24 @@ import java.util.List;
 public class ItemController {
 
     private final ItemService itemService;
-    private final String header = "X-Sharer-User-Id";
+    private static final String HEADER = "X-Sharer-User-Id";
 
     @PostMapping
-    public ItemDto createItem(@RequestHeader(header) long userId, @Validated({Marker.OnCreate.class}) @RequestBody ItemDto itemDto) {
+    public ItemDto createItem(@RequestHeader(HEADER) long userId, @Validated({Marker.OnCreate.class}) @RequestBody ItemDto itemDto) {
         Item item = itemService.createItem(userId, ItemMapper.toItem(itemDto));
         log.info("POST request for add item " + itemDto.toString() + "from user with id = " + userId);
         return ItemMapper.toItemDto(item);
     }
 
     @GetMapping("/{itemId}")
-    public ItemDto findItem(@RequestHeader(header) @Min(0) Long userId, @PathVariable("itemId") final Long itemId) {
+    public ItemDto findItem(@RequestHeader(HEADER) @Min(0) Long userId, @PathVariable("itemId") final Long itemId) {
         Item item = itemService.findItem(userId, itemId);
         log.info("GET request for find item with id = " + itemId + " from user with id = " + userId);
         return ItemMapper.toItemDto(item);
     }
 
     @PatchMapping("/{itemId}")
-    public ItemDto updateItem(@RequestHeader(header) @Min(0) Long userId, @PathVariable("itemId") final Long itemId,
+    public ItemDto updateItem(@RequestHeader(HEADER) @Min(0) Long userId, @PathVariable("itemId") final Long itemId,
                               @Validated({Marker.OnUpdate.class}) @RequestBody ItemDto itemDto) {
         Item item = itemService.updateItem(userId, itemId, ItemMapper.toItem(itemDto));
         log.info("PATCH request for update item id = " + itemId + " from user with id = " + userId);
@@ -46,14 +46,14 @@ public class ItemController {
     }
 
     @GetMapping
-    public List<ItemDto> findAllItemByUser(@RequestHeader(header) @Min(0) Long userId) {
+    public List<ItemDto> findAllItemByUser(@RequestHeader(HEADER) @Min(0) Long userId) {
         List<Item> listItem = itemService.findAllItemByUser(userId);
         log.info("GET request for find all items from user with id = " + userId);
         return ItemMapper.toListItemDto(listItem);
     }
 
     @GetMapping("/search")
-    public List<ItemDto> search(@RequestHeader(header) @Min(0) Long userId, @RequestParam String text) {
+    public List<ItemDto> search(@RequestHeader(HEADER) @Min(0) Long userId, @RequestParam String text) {
         List<Item> itemList = itemService.search(userId, text);
         log.info("GET request for find items");
         return ItemMapper.toListItemDto(itemList);
