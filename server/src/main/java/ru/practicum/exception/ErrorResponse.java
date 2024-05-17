73ef031c26665ba0c04b97a36f0d203e8fef7985
S.Petrovskii <sergdest261276@yandex.ru>
2024-05-17 19:16:33 +0300

    fix: ТЗ-16 Correct exceptions - 3

--- server/src/main/java/ru/practicum/exception/ErrorResponse.java
+++ server/src/main/java/ru/practicum/exception/ErrorResponse.java
@@ -3,16 +3,16 @@ package ru.practicum.exception;
 import lombok.Data;
 
 @Data
-public class ErrorResponseException {
+public class ErrorResponse {
     private String error;
     private String description;
 
-    public ErrorResponseException(String error, String description) {
+    public ErrorResponse(String error, String description) {
         this.error = error;
         this.description = description;
     }
 
-    public ErrorResponseException(String error) {
+    public ErrorResponse(String error) {
         this.error = error;
     }
 }
--- server/src/main/java/ru/practicum/exception/HandlerException.java
+++ server/src/main/java/ru/practicum/exception/HandlerException.java
@@ -9,42 +9,42 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
-@RestControllerAdvice("ru.practicum.shareit")
+@RestControllerAdvice("ru.practicum")
 @Slf4j
 public class HandlerException {
     @ExceptionHandler({EntityNotFoundException.class})
     @ResponseStatus(HttpStatus.NOT_FOUND)
-    public ErrorResponseException notFoundError(final RuntimeException e) {
+    public ErrorResponse notFoundError(final RuntimeException e) {
         log.debug("Get error 404 Not found {}", e.getMessage(), e);
-        return new ErrorResponseException("Not found", e.getMessage());
+        return new ErrorResponse("Not found", e.getMessage());
     }
 
     @ExceptionHandler({EmailDuplicateException.class})
     @ResponseStatus(HttpStatus.CONFLICT)
-    public ErrorResponseException duplicate(final RuntimeException e) {
+    public ErrorResponse duplicate(final RuntimeException e) {
         log.debug("Get error 409 Conflict {}", e.getMessage(), e);
-        return new ErrorResponseException("Duplicates was found", e.getMessage());
+        return new ErrorResponse("Duplicates was found", e.getMessage());
     }
 
     @ExceptionHandler({MissingRequestHeaderException.class, MethodArgumentNotValidException.class,
             AvailableException.class, CommentException.class})
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ErrorResponseException badRequest(final Exception e) {
+    public ErrorResponse badRequest(final Exception e) {
         log.debug("Get error 400 Bad Request {}", e.getMessage(), e);
-        return new ErrorResponseException("Bad Request", e.getMessage());
+        return new ErrorResponse("Bad Request", e.getMessage());
     }
 
     @ExceptionHandler({MethodArgumentTypeMismatchException.class, IllegalArgumentException.class})
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    public ErrorResponseException statusError(final Throwable e) {
+    public ErrorResponse statusError(final Throwable e) {
         log.debug("Get status 500 Internal Server Error {}", e.getMessage(), e);
-        return new ErrorResponseException(e.getMessage());
+        return new ErrorResponse("Unknown state: UNSUPPORTED_STATUS", e.getMessage());
     }
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    public ErrorResponseException internalServerError(final Throwable e) {
+    public ErrorResponse internalServerError(final Throwable e) {
         log.debug("Get error 500 Internal Server Error {}", e.getMessage(), e);
-        return new ErrorResponseException("Internal Server Error", e.getMessage());
+        return new ErrorResponse("Internal Server Error", e.getMessage());
     }
 }
