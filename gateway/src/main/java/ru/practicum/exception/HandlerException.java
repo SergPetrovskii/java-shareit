3f74dfda2dab3542ef962dadf2947f1c71f36f55
S.Petrovskii <sergdest261276@yandex.ru>
2024-05-18 22:41:05 +0300

    fix: ТЗ-16 Correct exceptions - 8-3

--- gateway/src/main/java/ru/practicum/exception/HandlerException.java
+++ gateway/src/main/java/ru/practicum/exception/HandlerException.java
@@ -25,6 +25,6 @@ public class HandlerException {
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public ErrorResponse statusError(final Throwable e) {
         log.debug("Get error 500 Internal Server Error {}", e.getMessage(), e);
-        return new ErrorResponse(e.getMessage());
+        return new ErrorResponse("Unknown state: UNSUPPORTED_STATUS");
     }
 }
